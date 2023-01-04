8aa9156a36857bbe40c10c367327e1efaf3b4c24
Régis S <regis@shares.io>
2023-01-04 15:44:47 +0100

    Add Timeout to be sure that application is launched

--- 03_StaticAnalysis/android-dex_classes_enumeration.js
+++ 03_StaticAnalysis/android-dex_classes_enumeration.js
@@ -1,28 +1,31 @@
 /* 
     Description: Android Dex Classes Enumeration
     Usage: frida -U -f XXX -l android-dex_classes_enumeration.js
-    Credit: @mobilesecurity_
+    Credit: @mobilesecurity_ & rsenet
 
     Link:
         https://developer.android.com/reference/dalvik/system/DexFile
 */
 
-Java.perform(function ()
+setTimeout(function() 
 {
-    console.log("Dex Classes Enumeration - started")
-    const ActivityThread = Java.use('android.app.ActivityThread');
-    const DexFile = Java.use("dalvik.system.DexFile");
-
-    var targetApp = ActivityThread.currentApplication();
-    var context = targetApp.getApplicationContext();
-    var apk_path = context.getPackageCodePath().toString()
-
-    var df = DexFile.$new(apk_path);
-    var dexClasses = df.entries()
-    
-    while (dexClasses.hasMoreElements()) 
+    Java.perform(function ()
     {
-        console.log(String(dexClasses.nextElement()))
-    }
-    console.log("Dex Classes Enumeration - completed")
-});
+        console.log("Dex Classes Enumeration - started")
+        const ActivityThread = Java.use('android.app.ActivityThread');
+        const DexFile = Java.use("dalvik.system.DexFile");
+
+        var targetApp = ActivityThread.currentApplication();
+        var context = targetApp.getApplicationContext();
+        var apk_path = context.getPackageCodePath().toString()
+
+        var df = DexFile.$new(apk_path);
+        var dexClasses = df.entries()
+        
+        while (dexClasses.hasMoreElements()) 
+        {
+            console.log(String(dexClasses.nextElement()))
+        }
+        console.log("Dex Classes Enumeration - completed")
+    });
+}, 5);
