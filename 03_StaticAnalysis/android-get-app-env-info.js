360c3c209ed13b7fb51cd96e91e50da68f291a9a
Régis S <regis@shares.io>
2023-01-04 15:38:16 +0100

    Replace send by console.log and add Timeout to be sure that application is launched

--- 03_StaticAnalysis/android-get-app-env-info.js
+++ 03_StaticAnalysis/android-get-app-env-info.js
@@ -1,43 +1,46 @@
 /* 
     Description: Android Application Get Env information
     Usage: frida -U -f XXX -l android-get-app-env-info.js
-    Credit: @mobilesecurity_
+    Credit: @mobilesecurity_ & rsenet
 */
 
-Java.perform(function() 
+setTimeout(function() 
 {
-    var context = null
-    var ActivityThread = Java.use('android.app.ActivityThread');
-    var targetApp = ActivityThread.currentApplication();
-
-    if (targetApp != null) 
+    Java.perform(function() 
     {
-        context = targetApp.getApplicationContext();
+        var context = null
+        var ActivityThread = Java.use('android.app.ActivityThread');
+        var targetApp = ActivityThread.currentApplication();
 
-        var env = 
+        if (targetApp != null) 
         {
-            mainDirectory: context.getFilesDir().getParent(),
-            filesDirectory: context.getFilesDir().getAbsolutePath().toString(),
-            cacheDirectory: context.getCacheDir().getAbsolutePath().toString(),
-            externalCacheDirectory: context.getExternalCacheDir().getAbsolutePath().toString(),
-            codeCacheDirectory: 
-                'getCodeCacheDir' in context ? 
-                context.getCodeCacheDir().getAbsolutePath().toString() : 'N/A',
-            obbDir: context.getObbDir().getAbsolutePath().toString(),
-            packageCodePath: context.getPackageCodePath().toString(),
-        };
+            context = targetApp.getApplicationContext();
 
-        send("******************* App Environment Info *******************")
-        send("mainDirectory: "+env.mainDirectory);
-        send("filesDirectory: "+env.filesDirectory);
-        send("cacheDirectory: "+env.cacheDirectory);
-        send("externalCacheDirectory: "+env.externalCacheDirectory);
-        send("codeCacheDirectory: "+env.codeCacheDirectory);
-        send("obbDir: "+env.obbDir);
-        send("packageCodePath: "+env.packageCodePath);
-        send("************************************************************")
-    } 
-    else 
-        console.log("Error: App Environment Info - N/A")
+            var env = 
+            {
+                mainDirectory: context.getFilesDir().getParent(),
+                filesDirectory: context.getFilesDir().getAbsolutePath().toString(),
+                cacheDirectory: context.getCacheDir().getAbsolutePath().toString(),
+                externalCacheDirectory: context.getExternalCacheDir().getAbsolutePath().toString(),
+                codeCacheDirectory: 
+                    'getCodeCacheDir' in context ? 
+                    context.getCodeCacheDir().getAbsolutePath().toString() : 'N/A',
+                obbDir: context.getObbDir().getAbsolutePath().toString(),
+                packageCodePath: context.getPackageCodePath().toString(),
+            };
 
-});
+            console.log("******************* App Environment Info *******************")
+            console.log("mainDirectory: " + env.mainDirectory);
+            console.log("filesDirectory: " + env.filesDirectory);
+            console.log("cacheDirectory: " + env.cacheDirectory);
+            console.log("externalCacheDirectory: " + env.externalCacheDirectory);
+            console.log("codeCacheDirectory: " + env.codeCacheDirectory);
+            console.log("obbDir: " + env.obbDir);
+            console.log("packageCodePath: " + env.packageCodePath);
+            console.log("************************************************************")
+        } 
+        else
+            console.log("Error: App Environment Info - N/A")
+
+    });
+}, 5);
