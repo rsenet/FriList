a97b2f94c140d68ad37b9f8683d39778f9a1e0ba
Régis S <regis@shares.io>
2023-01-31 18:06:37 +0100

    Update iOS WebView Observer

--- 01_Observer/Network/Webview/ios-webview-observer.js
+++ 01_Observer/Network/Webview/ios-webview-observer.js
@@ -30,8 +30,10 @@ if (ObjC.available)
         {
             console.log('onMatch: ', wk);
             console.log('URL: ', wk.URL().toString());
-            console.log('javaScriptEnabled:', wk.configuration().preferences().javaScriptEnabled());
+            console.log('javaScriptEnabled: ', wk.configuration().preferences().javaScriptEnabled());
+            console.log('allowFileAccessFromFileURLs: ', wk.configuration().preferences().valueForKey_('allowFileAccessFromFileURLs').toString());
             console.log('hasOnlySecureContent: ', wk.hasOnlySecureContent().toString());
+            console.log('allowUniversalAccessFromFileURLs: ', wk.configuration().valueForKey_('allowUniversalAccessFromFileURLs').toString());
         },
         onComplete: function () 
         {
